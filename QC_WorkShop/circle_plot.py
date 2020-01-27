3b2923863be4d7bfe3b5d413a9cd43d1f91764d7
David Radcliffe <dradcliffe@gmail.com>
2020-01-27 13:58:24 -0600

    Add documentation for circle_plot module; update .gitignore

--- QC_WorkShop/circle_plot.py
+++ QC_WorkShop/circle_plot.py
@@ -1,37 +1,31 @@
-# File: plot_circles.py
-#
-#   This code is used with Qiskit to display the circle plot representation of the state vector
-#   of a quantum register. The state vector for an N-qubit quantum register has 2^N components,
-#   which are called amplitudes. Each amplitude is a complex number, and the squared magnitudes
-#   add up to 1.
-#
-#   The circe plot has a circle of radius 1 for each amplitude, with a filled-in circle inside it.
-#   The radius of the filled-in circle is equal to the magnitude. The circle also contains a line
-#   segment from the center of the circle to the circumference. The line segment is rotated to show
-#   the relative phase.
-#
-#   EXAMPLE: (Bell state)
-#
-#       from qiskit import QuantumCircuit, QuantumRegister, execute, BasicAer
-#       from plot_circles import plot_circles
-#
-#       a = QuantumRegister(1, name='a')
-#       b = QuantumRegister(1, name='b')
-#       qc = QuantumCircuit(a, b)
-#       qc.h(a)
-#       qc.cx(a, b)
-#       backend = BasicAer.get_backend('statevector_simulator')
-#       job = execute(qc, backend)
-#       result = job.result()
-#       outputstate = result.get_statevector(qc, decimals=3)
-#       plot_circles(outputstate)
-#
-#  The circle plot representation is described in "Programming Quantum Computers"
-#  by Eric R. Johnson, Nic Harrigan, and Mercedes Gimeno-Segovia. It is also used
-#  in the QCEngine quantum simulator that accompanies the book.
-#
-#  See https://oreilly-qc.github.io
+"""
+This code is used with Qiskit to display the circle plot representation of the state vector
+of a quantum register. The state vector for an N-qubit quantum register has 2^N components,
+which are called amplitudes. Each amplitude is a complex number, and the squared magnitudes
+add up to 1.
 
+The circle plot has a circle of radius 1 for each amplitude, with a filled-in circle inside it.
+The radius of the filled-in circle is equal to the magnitude. The circle also contains a line
+segment from the center of the circle to the circumference. The line segment is rotated to show
+the relative phase.
+
+EXAMPLE: (Bell state)
+
+   > from qiskit import QuantumCircuit, QuantumRegister, execute, BasicAer 
+   > from circle_plot import plot_circles
+   > a = QuantumRegister(1, name='a') 
+   > b = QuantumRegister(1, name='b')
+   > qc = QuantumCircuit(a, b)
+   > qc.h(a)
+   > qc.cx(a, b)
+   > plot_circuit(qc)
+
+The circle plot representation is described in "Programming Quantum Computers"
+by Eric R. Johnson, Nic Harrigan, and Mercedes Gimeno-Segovia. It is also used
+in the QCEngine quantum simulator that accompanies the book.
+
+See https://oreilly-qc.github.io
+"""
 
 import matplotlib
 import matplotlib.pyplot as plt
@@ -40,9 +34,20 @@ import matplotlib.lines as mlines
 import numpy as np
 
 def plot_circle(axis, amplitude, index, **kwargs):
-    """Generates a circle representation for a single amplitude."""
+    """Generates a circle representation for a single amplitude.
+    
+    Parameters:
+        axis      -- A Matplotlib axis object, on which a circle is drawn.
+        amplitude -- A complex number of modulus 1 or less representing a quantum amplitude.
+        index     -- An integer between 0 and 2^N - 1 (N = # of qubits) indicating the position in the state vector.
+    
+    Keyword arguments:
+        long_phase -- if True, extend the phase line to the outer circle (default False)
+        fill       -- if True, fill in the inner circe (default True)
+        color      -- a string representing the color of the inner circle (default 'red')
+    """
     r = abs(amplitude)
-    if kwargs.get('long_phase') and r > 1e-6:
+    if kwargs.get('long_phase') and r > 1e-3:
         amplitude /= r
     color = kwargs.get('color', 'red')
     fill = kwargs.get('fill', True)
@@ -59,12 +64,25 @@ def plot_circle(axis, amplitude, index, **kwargs):
     axis.text(-0.2, -1.5, f'|{index}\u27e9')
 
 
-def plot_circles(state_vector, nrows=1, **kwargs):
-    """Generates the circle-plot representation for the state vector of a quantum register."""
+def plot_circles(state_vector, **kwargs):
+    """Generates the circle-plot representation for the state vector of a quantum register.
+    
+    Parameters:
+        state_vector -- A complex vector of norm 1 with 2^N components (N = # of qubits).
+    
+    Keyword arguments:
+        nrows    -- The number of rows in the circle diagram (default 1).
+        fontsize -- The font size for the captions, in pixels (default 12).
+    
+    Other keyword arguments are passed to the plot_circle function.
+    """
+    
+    N = len(state_vector)
+    assert N > 0 and N & (N - 1) == 0, 'Length of state_vector must be a power of 2.'
+    nrows = kwargs.get('nrows', 1)
+    assert 0 < nrows and N % nrows == 0, 'invalid value of nrows'
     fontsize = kwargs.get('fontsize', 12)
     matplotlib.rc('font', family='serif', weight='bold', size=fontsize)
-    N = len(state_vector)
-    assert 0 < nrows and N % nrows == 0, 'invalid value of nrows'
     ncols = N // nrows
     fig, axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=(2 * ncols, nrows + 1))
     for index in range(N):
@@ -78,3 +96,18 @@ def plot_circles(state_vector, nrows=1, **kwargs):
         amplitude = state_vector[index]
         plot_circle(axis, amplitude, index, **kwargs)
     plt.show()
+
+
+def plot_circuit(circuit, **kwargs):
+    """Simulate a quantum circuit in Qiskit and plot the state vector as a circle diagram.
+    
+    Parameters:
+         circuit -- A Qiskit QuantumCircuit object.
+    
+    Keyword arguments are passed to the plot_circles function.
+    """
+    backend = BasicAer.get_backend('statevector_simulator')
+    job = execute(qc, backend)
+    result = job.result()
+    outputstate = result.get_statevector(qc, decimals=3)
+    plot_circles(outputstate, **kwargs)
